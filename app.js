8154751a8a7eaed1bfd6c311987267803395dced
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-04-16 14:06:43 +0200

    can add users to DB from form

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 const hbs = require("hbs");
 const parser = require("body-parser");
 const mongoose = require('mongoose');
+const axios = require("axios");
 const Users = require('./models/Users'); // Import of the model User from './models/Users'
+app.use(parser.urlencoded({
+  extended: false
+}));
 
 mongoose.connect('mongodb://localhost/usersAPP', {
     useNewUrlParser: true
@@ -24,6 +28,29 @@ app.get("/", (req, res) => {
   res.render("home.hbs");
 });
 
+app.post("/user", (req, res) => {
+  var user = new Users(req.body);
+  console.log(user);
+  Users.create(user).then(userData => {
+    console.log("my user is in data base", userData);
+  }).catch(err => {
+    console.log(err, " Error");
+  })
+})
+
+
+
+// TO CREATE A NEW USER IN DB //
+// app.get("/home.hbs", (req, res) => {
+//   const data = {
+//     scripts: ["form_product.js", "list_products.js"]
+//   };
+//   res.Users.Create({
+// }).then(usersData => {
+//   console.log("The new user is in data base", usersData);
+// }).catch(err => {
+//   console.log(err, " Error");
+// })
 
 
 
---
+++ public/takedata.js
@@ -0,0 +1,4 @@
+document.getElementById("create_user").onsubmit = function () {
+  event.preventDefault();
+  console.log('new-user-form');
+}
