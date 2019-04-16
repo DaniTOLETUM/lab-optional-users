997001a752e037ec2d0300fb358d30466a141e71
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-04-16 10:05:25 +0200

    DBMongo set

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const hbs = require("hbs");
 const parser = require("body-parser");
+const mongoose = require('mongoose');
+const Users = require('./models/Users'); // Import of the model User from './models/Users'
+
+mongoose.connect('mongodb://localhost/usersAPP', {
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log('Connected to usersAPP from Mongo!');
+  }).catch(err => {
+    console.error('Error connecting to usersAPP from mongo', err);
+  });
 
 app.use(parser.json());
 app.set("views", __dirname + "/views");
